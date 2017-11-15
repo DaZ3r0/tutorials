3290299c8d1ddde2679c228546f285658406c5c7
DaZ3r0 <darshan.chaluvaraju@gmail.com>
2017-11-16 02:20:34 +0530

    Update dockerise.sh

--- mesos-marathon/dockerise.sh
+++ mesos-marathon/dockerise.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
-docker login -u darshan.chaluvaraju@gmail.com -p $DOCKER_PASSWORD
+docker login -u darshandocker -p $DOCKER_PASSWORD
 docker build -t baeldung/mesos-marathon-demo:$BUILD_NUMBER .
 docker push baeldung/mesos-marathon-demo:$BUILD_NUMBER
